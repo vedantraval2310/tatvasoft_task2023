2a1665bf64b2e6f1299927798264a344107413a8
Vedant Raval <76436755+vedantraval2310@users.noreply.github.com>
2023-03-26 22:13:22 +0530

    week4

---
+++ week4_task/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PostDetail from "./components/PostDetail";
+import RegisterUser from "./components/RegisterUser";
+import Users from "./components/User";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <h1>Week 4 PostDetails</h1>
+      </header>
+      <PostDetail />
+      <header className="App-header">
+        <h1>Week 4 day 19 RegisterUser</h1>
+      </header>
+      <RegisterUser/>
+      <header className="App-header">
+        <h1>Week 4 day 19 task 2 UserDetail using memo</h1>
+      </header>
+      <Users/>
+      <header className="App-header">
+        <h1>Week 4 day 20 task 2 Form and debounce method</h1>
+      </header>
+      <Form/>
+    </div>
+   
+  );
+}
+
+export default App;
